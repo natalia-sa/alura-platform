9c42346677e8f65932ec6cafb75d36962c188d6e
Natalia Salvino <nataliasalvino86@gmail.com>
2024-03-23 01:52:49 -0300

    feat: adding swagger docs

--- src/main/java/com/alura/platform/user/UserController.java
+++ src/main/java/com/alura/platform/user/UserController.java
@@ -1,6 +1,11 @@
 package com.alura.platform.user;
 
 import com.alura.platform.user.dto.UserDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +13,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.awt.print.Book;
 
 @RestController
 @RequestMapping("/user")
@@ -17,6 +24,13 @@ public class UserController {
     private UserService userService;
 
     @PostMapping(value = "")
+    @Operation(summary = "Save new user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "User was created successfully",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = User.class)) }),
+            @ApiResponse(responseCode = "500", description = "Something went wrong while creating user",
+                    content = @Content) })
     public ResponseEntity save(
             @RequestBody
             UserDto userDto) {
--- src/main/java/com/alura/platform/user/dto/UserDto.java
+++ src/main/java/com/alura/platform/user/dto/UserDto.java
@@ -1,6 +1,21 @@
 package com.alura.platform.user.dto;
 
 import com.alura.platform.user.enums.UserRoleEnum;
+import io.swagger.v3.oas.annotations.media.Schema;
 
-public record UserDto(String name, String username, String email, String password, UserRoleEnum role) {
+public record UserDto(
+        @Schema(example = "João")
+        String name,
+
+        @Schema(example = "joao")
+        String username,
+
+        @Schema(example = "joao@gmail.com")
+        String email,
+
+        @Schema(example = "joao123")
+        String password,
+
+        @Schema(example = "STUDENT")
+        UserRoleEnum role) {
 }
