c8a286ebde0fb2e94c026853a8e1f616abdd2f5f
Natalia Salvino <nataliasalvino86@gmail.com>
2024-03-24 16:51:59 -0300

    feat: sending email to instructor if review rating is less than 6

--- src/main/java/com/alura/platform/business/course_review/service/CourseReviewServiceImpl.java
+++ src/main/java/com/alura/platform/business/course_review/service/CourseReviewServiceImpl.java
@@ -6,11 +6,13 @@ import com.alura.platform.business.course.service.CourseService;
 import com.alura.platform.business.course_review.dto.CourseReviewDto;
 import com.alura.platform.business.course_review.entity.CourseReview;
 import com.alura.platform.business.course_review.repository.CourseReviewRepository;
+import com.alura.platform.business.email.EmailSender;
 import com.alura.platform.business.registration.entity.Registration;
 import com.alura.platform.business.registration.service.RegistrationService;
 import com.alura.platform.business.user.entity.User;
 import com.alura.platform.business.user.service.UserService;
 import com.alura.platform.exception.ActionDeniedException;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
@@ -38,6 +40,7 @@ class CourseReviewServiceImpl implements CourseReviewService {
     }
 
     @Override
+    @Transactional
     public CourseReview save(CourseReviewDto courseReviewDto) {
         User user = userService.findById(courseReviewDto.userId()).orElseThrow();
         Course course = courseService.findById(courseReviewDto.courseId()).orElseThrow();
@@ -55,6 +58,12 @@ class CourseReviewServiceImpl implements CourseReviewService {
         }
 
         CourseReview courseReview = new CourseReview(user, course, courseReviewDto.rating(), courseReviewDto.comment());
+
+        if (courseReview.getRating() < 6) {
+            User instructor = userService.findById(course.getInstructor().getId()).orElseThrow();
+            sendEmailNotificationToInstructor(courseReview, instructor, course);
+        }
+
         return courseReviewRepository.save(courseReview);
     }
 
@@ -65,6 +74,19 @@ class CourseReviewServiceImpl implements CourseReviewService {
     private boolean checkIfUserIsRegisteredInCourse(Long userId, Long courseId) {
         Optional<Registration> registrationOptional = registrationService.findByUserIdCourseId(userId, courseId);
         return registrationOptional.isPresent();
+    }
+
+    private void sendEmailNotificationToInstructor(CourseReview courseReview, User instructor, Course course) {
+        String recipientEmail = instructor.getEmail();
+        String courseName = course.getName();
+        String subject = "New review - course %s".formatted(courseName);
+        String body = "Course %s received new review. %nRating: %d".formatted(courseName, courseReview.getRating());
+
+        if(courseReview.getComment() != null) {
+            body += "%nComment: %s".formatted(courseReview.getComment());
+        }
+
+        EmailSender.send(recipientEmail, subject, body);
     }
 
 }
---
+++ src/main/java/com/alura/platform/business/email/EmailSender.java
@@ -0,0 +1,14 @@
+package com.alura.platform.business.email;
+
+public class EmailSender {
+
+    public static void send(String recipientEmail, String subject, String body) {
+        System.out.println(
+                "Simulating sending email to [%s]:\n".formatted(recipientEmail)
+        );
+        System.out.println("""
+            Subject: %s
+            Body: %s
+            """.formatted(subject, body));
+    }
+}
