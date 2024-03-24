a4ee9052805bffc528d0def0ece5ddd538b2d1e0
Natalia Salvino <nataliasalvino86@gmail.com>
2024-03-24 13:30:12 -0300

    test: adding tests to save registration endpoint and service function

---
+++ src/test/java/com/alura/platform/business/registration/controller/SaveTest.java
@@ -0,0 +1,63 @@
+package com.alura.platform.business.registration.controller;
+
+import com.alura.platform.basic.BasicControllerTest;
+import com.alura.platform.business.registration.dto.RegistrationUserIdCourseIdDto;
+import com.alura.platform.business.registration.entity.Registration;
+import com.alura.platform.business.registration.service.RegistrationService;
+import com.alura.platform.exception.ActionDeniedException;
+import com.google.gson.Gson;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+class SaveTest extends BasicControllerTest {
+
+
+    private static final String PATH = "/registration";
+
+    private final Gson gson = new Gson();
+
+    @MockBean
+    private RegistrationService registrationService;
+
+    @Test
+    @DisplayName("Should return 201 when registration was created with success")
+    void shouldReturnSuccessWhenRegistrationWasCreatedSuccessfullyTest() throws Exception {
+        RegistrationUserIdCourseIdDto registrationDto = new RegistrationUserIdCourseIdDto(1L, 1L);
+
+        Registration registration = new Registration();
+
+        Mockito.when(this.registrationService.save(registrationDto)).thenReturn(registration);
+
+        callEndpoint(registrationDto).andExpect(status().isCreated());
+
+        Mockito.verify(this.registrationService, Mockito.times(1)).save(registrationDto);
+    }
+
+    @Test
+    @DisplayName("Should return 403 when ActionDeniedException is thrown")
+    void shouldReturnActionDeniedWhenActionDeniedExceptionIsThrownTest() throws Exception {
+        RegistrationUserIdCourseIdDto registrationDto = new RegistrationUserIdCourseIdDto(1L, 1L);
+
+        Mockito.when(registrationService.save(registrationDto))
+                .thenThrow(new ActionDeniedException("Course is not active"));
+
+        callEndpoint(registrationDto).andExpect(status().isForbidden());
+    }
+
+    private ResultActions callEndpoint(RegistrationUserIdCourseIdDto registrationDto) throws Exception {
+        String json = gson.toJson(registrationDto);
+
+        return mockMvc.perform(MockMvcRequestBuilders
+                .post(PATH)
+                .content(json)
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON));
+    }
+}
---
+++ src/test/java/com/alura/platform/business/registration/service/SaveTest.java
@@ -0,0 +1,55 @@
+package com.alura.platform.business.registration.service;
+
+import com.alura.platform.business.course.entity.Course;
+import com.alura.platform.business.course.enums.CourseStatusEnum;
+import com.alura.platform.business.course.service.CourseService;
+import com.alura.platform.business.registration.dto.RegistrationUserIdCourseIdDto;
+import com.alura.platform.business.registration.entity.Registration;
+import com.alura.platform.business.user.entity.User;
+import com.alura.platform.business.user.enums.UserRoleEnum;
+import com.alura.platform.business.user.service.UserService;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles({"test"})
+class SaveTest {
+
+    @Autowired
+    private CourseService courseService;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private RegistrationService registrationService;
+
+    @AfterEach
+    @Transactional
+    void cleanDatabase() {
+        registrationService.deleteAll();
+        courseService.deleteAll();
+        userService.deleteAll();
+    }
+
+    @Test
+    @DisplayName("Should save registration successfully")
+    void shouldSaveRegistrationTest() {
+        User instructor = userService.save(new User("name", "username", "user@gmail.com", "password", UserRoleEnum.INSTRUCTOR));
+        User user = userService.save(new User("name", "newuser", "user2@gmail.com", "password", UserRoleEnum.STUDENT));
+
+        Course course = courseService.save(new Course("name", "code", instructor, "description", CourseStatusEnum.ACTIVE));
+
+        RegistrationUserIdCourseIdDto registrationDto = new RegistrationUserIdCourseIdDto(user.getId(), course.getId());
+        Registration registration = registrationService.save(registrationDto);
+
+        Assertions.assertEquals(1, registrationService.findAll().size());
+        Assertions.assertNotNull(registration.getId());
+    }
+}
